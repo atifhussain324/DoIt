3a345835363176960e110a5977819336aef4dd5b
Atif Hussain <atifhussain324@gmail.com>
2017-07-08 21:29:40 -0400

    Write method added for creating new tasks to db, ToDo class refactored to Task class

--- app/src/main/java/com/example/atif/todolist/NewTaskActivity.java
+++ app/src/main/java/com/example/atif/todolist/NewTaskActivity.java
@@ -1,24 +1,48 @@
 package com.example.atif.todolist;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.sql.Time;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
-public class NewTaskActivity extends AppCompatActivity {
+public class NewTaskActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
     Calendar myCalendar = Calendar.getInstance();
-    EditText dueDate;
+    EditText editDueDate;
+    EditText editDueTime;
+    EditText editTitle;
+    EditText editDescription;
+    String type;
+    String title;
+    String description;
+    String dueDate;
+    String dueTime;
+
+    private DatabaseReference mDatabase;
+
     DatePickerDialog.OnDateSetListener date;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,23 +51,29 @@ public class NewTaskActivity extends AppCompatActivity {
         toolbar.setTitle("New Task");
         setSupportActionBar(toolbar);
 
+        editTitle = (EditText) findViewById(R.id.title);
+        editDescription = (EditText) findViewById(R.id.description);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("Tasks");
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                title = editTitle.getText().toString();
+                description = editDescription.getText().toString();
+                dueDate = editDueDate.getText().toString();
+                dueTime = editDueTime.getText().toString();
+                Log.v("Task",title + description + dueDate + dueTime);
+                saveTask();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
         date = new DatePickerDialog.OnDateSetListener() {
-
             @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
-                // TODO Auto-generated method stub
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
@@ -51,27 +81,97 @@ public class NewTaskActivity extends AppCompatActivity {
             }
 
         };
-        dueDate = (EditText) findViewById(R.id.duedate);
-        dueDate.setOnClickListener(new View.OnClickListener() {
-
+        //Date dialog
+        editDueDate = (EditText) findViewById(R.id.duedate);
+        editDueDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
                 new DatePickerDialog(NewTaskActivity.this, date, myCalendar
                         .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+        editDueTime = (EditText) findViewById(R.id.duetime);
+        editDueTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(NewTaskActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        editDueTime.setText(selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, false);
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+
+            }
+        });
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.task_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
     }
 
-        private void updateLabel() {
+    private void updateLabel() {
 
-            String myFormat = "MM/dd/yy"; //In which you need put here
-            SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
-            dueDate.setText(sdf.format(myCalendar.getTime()));
-        }
+        editDueDate.setText(sdf.format(myCalendar.getTime()));
     }
+
+
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        type = parent.getSelectedItem().toString();
+        Log.v("Type",type);
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+    public void saveTask(){
+        Task newTask = new Task();
+
+        newTask.setTitle(title);
+        newTask.setDescription(description);
+        newTask.setDueDate(dueDate);
+        newTask.setDueTime(dueTime);
+        newTask.setType(type);
+
+        String key = mDatabase.push().getKey();
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put(key, newTask);
+        mDatabase.updateChildren(childUpdates, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                if (databaseError == null) {
+                    finish();
+                }
+            }
+        });
+
+    }
+
+
+
+
+}
 
 
 
--- app/src/main/java/com/example/atif/todolist/Task.java
+++ app/src/main/java/com/example/atif/todolist/Task.java
@@ -6,10 +6,11 @@ import java.util.HashMap;
  * Created by Atif on 7/6/17.
  */
 
-public class ToDo {
+public class Task {
     private String title;
     private String description;
     private String dueDate;
+    private String dueTime;
     private String reminder;
     private String type;
     private String taskID;
@@ -38,6 +39,13 @@ public class ToDo {
     public void setDueDate(String dueDate) {
         this.dueDate = dueDate;
     }
+    public String getDueTime() {
+        return dueTime;
+    }
+
+    public void setDueTime(String dueTime) {
+        this.dueTime = dueTime;
+    }
     public String getReminder() {
         return title;
     }
@@ -61,8 +69,8 @@ public class ToDo {
     }
 
 
-/*
-    public HashMap<String,String> toFirebaseObject() {
+
+  /*  public HashMap<String,String> toFirebaseObject() {
         HashMap<String,String> todo =  new HashMap<String,String>();
         todo.put("name", name);
         todo.put("message", message);
